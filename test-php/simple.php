37a3cbc7795f4bdb2f4d7656e75aa8526b3619c4
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-10-25 16:09:50 +0100

    Add another test

---
+++ test-php/simple.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This is a simple but fully formed PHP definition.
+ *
+ * @param string $bar
+ * @return void
+ */
+function foo(string $bar): void {
+    echo $bar . "\n";
+}
--- test.js
+++ test.js
@@ -5,3 +5,7 @@ const PHPSemanticParser = require("./index")
 const trivial_test = fs.readFileSync(__dirname + "/test-php/trivial.php", {encoding: "utf8"})
 const p = new PHPSemanticParser(trivial_test)
 assert.ok(p.parse().nodes[0].nodes[0], "Trivial parse works")
+
+const simple_test = fs.readFileSync(__dirname + "/test-php/simple.php", {encoding: "utf8"})
+const p2 = new PHPSemanticParser(simple_test)
+assert.ok(p2.parse(), "Simple parse works")
