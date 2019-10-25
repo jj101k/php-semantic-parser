91c38f95021b50ce71088e21b3aa62d966290bfc
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-10-25 16:08:00 +0100

    More assertive test

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
+const assert = require("assert")
 const fs = require("fs")
 const PHPSemanticParser = require("./index")
 
-const simple_test = fs.readFileSync(__dirname + "/test-php/trivial.php", {encoding: "utf8"})
-const p = new PHPSemanticParser(simple_test)
-console.log(p.parse())
+const trivial_test = fs.readFileSync(__dirname + "/test-php/trivial.php", {encoding: "utf8"})
+const p = new PHPSemanticParser(trivial_test)
+assert.ok(p.parse().nodes[0].nodes[0], "Trivial parse works")
