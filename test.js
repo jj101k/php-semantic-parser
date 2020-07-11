87879fc5f7eabe96faf29c9494f61324403a2f7f
Jim Driscoll <jim.a.driscoll@gmail.com>
2020-07-11 15:59:56 +0100

    Report specifically where those warnings occur

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ const PHPSemanticParser = require("./index")
 
 const trivial_test = fs.readFileSync(__dirname + "/test-php/trivial.php", {encoding: "utf8"})
 const p = new PHPSemanticParser(trivial_test)
-assert.ok(p.parse().nodes[0].nodes[0], "Trivial parse works")
+assert.ok(p.parse(__dirname + "/test-php/trivial.php").nodes[0].nodes[0], "Trivial parse works")
 
 const simple_test = fs.readFileSync(__dirname + "/test-php/simple.php", {encoding: "utf8"})
 const p2 = new PHPSemanticParser(simple_test)
-assert.ok(p2.parse(), "Simple parse works")
+assert.ok(p2.parse(__dirname + "/test-php/simple.php"), "Simple parse works")
